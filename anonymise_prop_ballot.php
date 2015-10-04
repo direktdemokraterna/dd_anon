d3d6df1108b03ec32dbe7c3abb3b18e390f629f4
joakimsigvald <joakim.sigvald@gmail.com>
2015-10-04 19:26:06 +0200

    Fixed problem with user_code not being set on login
    Redesigned user_code regitration:
    Added config-variable for main server url
    some refactoring - removed some unneccessary empty lines and brackets
    and simpified some expressions.

--- anonymise_prop_ballot.php
+++ anonymise_prop_ballot.php
@@ -3,34 +3,18 @@
 require("init.inc");
 
 if(isset($_POST['user_id']) && isset($_POST['prop_id']) && isset($_POST['ballot'])){
-
 	$active_user_code = db_anon::get_active_user_code($_POST['user_id']);
-
 	$ballot_decoded = decrypt_ballot($_POST['ballot']);
-
 	if($active_user_code == $ballot_decoded['user_code']){
-
 		if($ballot_decoded['user_id'] == $_POST['user_id']){
 			$post_vars = Array("user_code" => $active_user_code, "prop_id" => $_POST['prop_id'], "rsa" => $ballot_decoded['rsa'], "aes" => $ballot_decoded['aes']);
-
 			$reply = curl_to_main_server("receive_prop_ballot.php", $post_vars);
-
-			if($reply == "success"){
-				echo "success";
-			}
-			else{
-				echo "failed - reply from receive_ballot: " . $reply;
-			}
+			echo $reply == "success" ? "success" : "failed - reply from receive_ballot: " . $reply;
 		}
-		else{
-			echo "failed - user_id mismatch";
-		}
-
+		else echo "failed - user_id mismatch";
 	}
-	else{
-		echo "failed - user code error";
-	}
-
+	else echo "failed - user code error: stored in ballot was " . $ballot_decoded['user_code'] . " but expected " . $active_user_code;
 }
+else echo "user_id, prop_id or ballot was not posted";
 
 ?>
--- give_out_user_codes.php
+++ give_out_user_codes.php
@@ -1,22 +1,16 @@
 <?php
 
-//echo "<meta content=\"application/javascript\">";
-
 require("init.inc");
-
 if(isset($_GET['user_id']) && isset($_GET['temp_code'])){
-
 	$user_codes = db_anon::get_user_codes($_GET['user_id'], $_GET['temp_code']);
-
 	if(!empty($user_codes)){
 		$output = Array("active_user_code" => $user_codes['active_user_code'], "passive_user_code1" => $user_codes['passive_user_code1'], "passive_user_code2" => $user_codes['passive_user_code2'], "passive_user_code3" => $user_codes['passive_user_code3']);
 		$output_encoded = json_encode($output);
-		echo "JSONP(" . $output_encoded . ")";
+		echo $output_encoded;
 	}
 	else{
 		echo "";
 	}
-
 }
 
 ?>
