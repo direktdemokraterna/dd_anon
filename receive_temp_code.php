e7f22dee1c0094eb426c86b90960f7b200c084e6
joakimsigvald <joakim.sigvald@gmail.com>
2015-10-06 23:05:57 +0200

    Generate new user codes on first login
    - fixed some errors after refactoring of db
    - made $db private static class property so it can be created in connect
    and then used elsewhere without passing it around
    - seems to have been an error where insert returns new id, but "ok" was
    expected
    - db_anon: removed unused method: insert_temp_code_and_constituencies
    - generate user_codes just before updating temp_code if user was not
    previously stored
    helpers.inc: got index out of bounds error and changed code to make that
    error more expressive next time it occurs (seems to be an off-by-one
    problem in the index randomization)
    remoed unused file new_user.php and moved functionality to db_anon (see
    above)
    replaced error codes with exception hanling in some places

--- receive_temp_code.php
+++ receive_temp_code.php
@@ -1,21 +1,11 @@
 <?php
-
 require("init.inc");
-
-//if(isset($_POST['user_id']) && isset($_POST['temp_code']) && isset($_POST['constituencies'])){
 if(isset($_POST['user_id']) && isset($_POST['temp_code'])){
-
-//	$constituencies_array = json_decode($_POST['constituencies'])
-
-//	$ok = db_anon::insert_temp_code_and_constituencies($_POST['temp_code'], $_POST['user_id'], $constituencies_array);
-
-	$ok = db_anon::insert_user_id_and_temp_code($_POST['user_id'], $_POST['temp_code']);
-
-	if($ok){
+	try {
+		db_anon::update_user_temp_code($_POST['user_id'], $_POST['temp_code']);
 		echo "success";
 	}
-	else{
-		echo "failed";
+	catch (Exception $e) {
+		echo "fail: " . $e;		
 	}
-
 }
