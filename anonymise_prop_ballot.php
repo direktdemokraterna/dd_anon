c749d69fe736ad3233ef9b2c8f28a8c36682b2d1
joakimsigvald <joakim.sigvald@gmail.com>
2015-11-07 07:13:09 +0100

    minor fixes
    - off by one error
    - better error message

--- anonymise_prop_ballot.php
+++ anonymise_prop_ballot.php
@@ -9,7 +9,9 @@ if(isset($_POST['user_id']) && isset($_POST['prop_id']) && isset($_POST['ballot'
 		if($ballot_decoded['user_id'] == $_POST['user_id']){
 			$post_vars = Array("user_code" => $active_user_code, "prop_id" => $_POST['prop_id'], "rsa" => $ballot_decoded['rsa'], "aes" => $ballot_decoded['aes']);
 			$reply = curl_to_main_server("receive_prop_ballot.php", $post_vars);
-			echo $reply == "success" ? "success" : "failed - reply from receive_ballot: " . $reply;
+			echo $reply == "success" 
+				? "success" 
+				: "failed - reply from receive_prop_ballot: " . $reply;
 		}
 		else echo "failed - user_id mismatch";
 	}
