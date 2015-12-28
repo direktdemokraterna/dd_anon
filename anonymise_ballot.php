6cd28abb122609d21fdbfcbab1c742fc13dc9f09
joakimsigvald <joakim.sigvald@gmail.com>
2015-12-28 14:04:04 +0100

    refactoring

--- anonymise_ballot.php
+++ anonymise_ballot.php
@@ -3,32 +3,17 @@
 require("init.inc");
 
 if(isset($_POST['user_id']) && isset($_POST['vote_id']) && isset($_POST['ballot'])){
-
 	$active_user_code = db_anon::get_active_user_code($_POST['user_id']);
-
 	$ballot_decoded = decrypt_ballot($_POST['ballot']);
-
 	if($active_user_code == $ballot_decoded['user_code']){
-
 		if($ballot_decoded['user_id'] == $_POST['user_id']){
 			$post_vars = Array("user_code" => $active_user_code, "vote_id" => $_POST['vote_id'], "rsa" => $ballot_decoded['rsa'], "aes" => $ballot_decoded['aes']);
-
 			$reply = curl_to_main_server("receive_ballot.php", $post_vars);
-
-			if($reply == "success"){
-				echo "success";
-			}
-			else{
-				echo "failed - reply from receive_ballot: " . $reply;
-			}
+			echo $reply == "success"
+				? "success"
+				: "failed - reply from receive_ballot: " . $reply;
 		}
-		else{
-			echo "failed - user_id mismatch";
-		}
-
+		else echo "failed - user_id mismatch";
 	}
-	else{
-		echo "failed - user code error";
-	}
-
+	else echo "failed - user code error";
 }
