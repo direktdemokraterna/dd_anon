1cfda34480e8c905da2f34aa647ccc6646d9964c
joakimsigvald <joakim.sigvald@gmail.com>
2015-12-28 23:23:15 +0100

    https://trello.com/c/tQylIwZ9/162-nar-man-tar-bort-en-medlem-tas-inte-dess-rost-bort-i-aktiv-proposition
    remove ballots from votes

--- remove_ballots.php
+++ remove_ballots.php
@@ -5,10 +5,10 @@ require("init.inc");
 //TODO: Add signature for web-server so anon-server can verify the sender of the request
 if(isset($_POST['user_id'])){
 	$user_codes = db_anon::get_all_user_codes($_POST['user_id']);
-	$reply = curl_to_main_server("remove_prop_ballots.php", $user_codes);
+	$reply = curl_to_main_server("remove_ballots.php", $user_codes);
 	echo $reply == "success" 
 		? "success" 
-		: "failed - reply from remove_prop_ballots: " . $reply;
+		: "failed - reply from remove_ballots: " . $reply;
 }
 else echo "user_id was not posted";
 
