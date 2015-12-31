43905b9717484f9629f38351c2db5766d9b5c589
joakimsigvald <joakim.sigvald@gmail.com>
2015-12-31 12:49:23 +0100

    dd and dd_anon exchenge public keys

--- receive_public_key.php
+++ receive_public_key.php
@@ -1,6 +1,6 @@
 <?php
 if(isset($_POST['key'])) {
-	$pk_filename = 'dd_public_key.txt';
+	$pk_filename = 'keys/dd/dd_public_key.txt';
 	if (file_put_contents($pk_filename, $_POST['key']))
 		echo "success";
 	else 
---
+++ retrieve_public_key.php
@@ -0,0 +1,4 @@
+<?php
+	$pk = file_get_contents("keys/dd_anon_public_key.txt");
+	echo $pk;
+?>
