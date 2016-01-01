74f738fa8617ad0e1173444eaf1cd023831e8da3
joakimsigvald <joakim.sigvald@gmail.com>
2016-01-01 20:36:03 +0100

    remove votes and prop-suport for user when she lose constituency access

--- remove_prop_ballots.php
+++ remove_prop_ballots.php
@@ -2,13 +2,14 @@
 
 require("init.inc");
 $data = get_signed_data();
-if(isset($data['user_id'])){
-	$user_codes = db_anon::get_all_user_codes($data['user_id']);
-	$reply = curl_to_main_server("remove_prop_ballots.php", $user_codes);
+if(isset($data['user_id']) && isset($data['constituency_id'])){
+	$post_vars = db_anon::get_all_user_codes($data['user_id']);
+	$post_vars['constituency_id'] = $data['constituency_id'];
+	$reply = curl_to_main_server("remove_prop_ballots.php", $post_vars);
 	echo $reply == "success" 
 		? "success" 
 		: "failed - reply from remove_prop_ballots: " . $reply;
 }
-else echo "user_id was not posted";
+else echo "user_id or constituency_id was not posted";
 
 ?>
