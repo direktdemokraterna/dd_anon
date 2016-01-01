be31d169df9a642edc0d4a88b480e9457b25be40
joakimsigvald <joakim.sigvald@gmail.com>
2016-01-01 19:36:35 +0100

    create foreign public key folder if not exists

--- receive_public_key.php
+++ receive_public_key.php
@@ -1,6 +1,9 @@
 <?php
 if(isset($_POST['key'])) {
-	$pk_filename = 'keys/dd/dd_public_key.txt';
+	$pk_foreign_key_path = 'keys/dd/';
+	$pk_filename = $pk_foreign_key_path . 'dd_public_key.txt';
+	if (!file_exists($pk_foreign_key_path))
+    	mkdir($pk_foreign_key_path, 0777, true);
 	if (file_put_contents($pk_filename, $_POST['key']))
 		echo "success";
 	else 
