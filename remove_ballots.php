e26d7eb977bf925bd634b1edfed285cc07bc0f0b
joakimsigvald <joakim.sigvald@gmail.com>
2016-01-06 12:52:10 +0100

    Remove votes when user or delegate looses constituency access

--- remove_ballots.php
+++ remove_ballots.php
@@ -1,15 +1,6 @@
 <?php
 
 require("init.inc");
-$data = get_signed_data();
-if(isset($data['user_id']) && isset($data['constituency_id'])){
-	$post_vars = db_anon::get_all_user_codes($data['user_id']);
-	$post_vars['constituency_id'] = $data['constituency_id'];
-	$reply = curl_to_main_server("remove_ballots.php", $post_vars);
-	echo $reply == "success" 
-		? "success" 
-		: "failed - reply from remove_ballots: " . $reply;
-}
-else echo "user_id or constituency_id was not posted";
+forward_command_for_user_and_constituency_with_user_codes("remove_ballots.php");
 
 ?>
---
+++ remove_delegations.php
@@ -0,0 +1,4 @@
+<?php
+require("init.inc");
+forward_command_for_user_and_constituency_with_user_codes("remove_delegations.php");
+?>
